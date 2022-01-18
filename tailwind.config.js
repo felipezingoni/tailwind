3b513acd0113998e09f2d2a5ae10532bfd1b6f40
Felipe Zingoni <felipezingoni.fz@gmail.com>
2022-01-18 16:28:43 -0300

    Hompage

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./app/**/*.{html,js,erb}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'banner-image': "url('https://i.imgur.com/zVZwT8e.png')",
+      },
+      colors: {
+        'green2': {
+          100: '#5EA779',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
